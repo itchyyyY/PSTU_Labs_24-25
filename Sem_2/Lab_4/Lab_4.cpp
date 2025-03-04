38be5355ba41e0b9ed107fdff9e4084f0cea2213
Danil Melnikov <formyself225@gmail.com>
2025-03-04 10:14:50 +0500

    +new files, +modified

--- Sem_2/Lab_4/Lab_4.cpp
+++ Sem_2/Lab_4/Lab_4.cpp
@@ -1,106 +1,49 @@
-#include <random>
 #include <iostream>
+#include <random>
+#include <clocale>
 
 using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "ru");
+
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<> dist(1, 100);
     
-    int n, n1, n2, tmp;
+    int arr[10]{};
 
-    cout << "Enter N: ";
-    cin >> n;
-
-    if (n < 14)
+    for (int i = 0; i < 10; ++i)
     {
-        cout << "N cannot be less than 14 !";
+        arr[i] = dist(gen);
+        cout << arr[i] << ' ';
     }
-    else
+    cout << endl;
+
+    int arr_1[5];
+
+    for (int i = 0; i < 5; ++i)
     {
-        n1 = n - 1;
-
-        int* arr = new int[n] {};
-        int* arr1 = new int[n1] {};
-
-        for (int i = 0; i < n; ++i)
-        {
-            arr[i] = dist(gen);
-        }
-
-        for (int i = 0; i < n; ++i)
-        {
-            cout << arr[i] << ' ';
-        }
-
-        cout << endl;
-
-        if (n % 2 == 0)
-        {
-            for (int i = 0; i < n / 2; ++i)
-            {
-                tmp = arr[i];
-                arr[i] = arr[n - i - 1];
-                arr[n - i - 1] = tmp;
-            }
-
-            n2 = n + 3;
-            int* arr2 = new int[n2] {};
-            for (int i = 0; i < 3; ++i)
-            {
-                arr2[i] = arr[i + 10] - 2;
-            }
-            for (int i = 3; i < n2; ++i)
-            {
-                arr2[i] = arr[i - 3];
-            }
-            for (int i = 0; i < n2; ++i)
-            {
-                cout << arr2[i] << ' ';
-            }
-            delete[]arr2;
-        }
-        else
-        {
-            for (int i = 0; i < n / 2; i++)
-            {
-                arr1[i] = arr[i];
-            }
-
-            for (int i = (n / 2); i < n1; i++)
-            {
-                arr1[i] = arr[i + 1];
-            }
-
-            for (int i = 0; i < n1 / 2; ++i)
-            {
-                tmp = arr1[i];
-                arr1[i] = arr1[n1 - i - 1];
-                arr1[n1 - i - 1] = tmp;
-            }
-
-            n2 = n1 + 3;
-            int* arr2 = new int[n2] {};
-            for (int i = 0; i < 3; ++i)
-            {
-                arr2[i] = arr1[i + 10] - 2;
-            }
-            for (int i = 3; i < n2; ++i)
-            {
-                arr2[i] = arr1[i - 3];
-            }
-            for (int i = 0; i < n2; ++i)
-            {
-                cout << arr2[i] << ' ';
-            }
-            delete[]arr1;
-            delete[]arr2;
-        }
-
-        delete[]arr;
+        arr_1[i] = arr[i + 5];
+        cout << arr_1[i] << ' ';
     }
+    cout << endl;
+
+    int arr_2[8];
+
+    for (int i = 0; i < 5; ++i)
+    {
+        arr_2[i] = arr_1[i];
+        cout << arr_2[i] << ' ';
+    }
+
+    for (int i = 5; i < 8; ++i)
+    {
+        arr_2[i] = dist(gen);
+        cout << arr_2[i] << ' ';
+    }
+    cout << endl;
     
 	return 0;
 }
