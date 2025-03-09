ebff1a6434caf6a6ff5c653d1ccf2d6ebcf457cf
Danil Melnikov <formyself225@gmail.com>
2025-03-09 13:40:40 +0500

    +modified

--- Sem_2/singly_linked_list/singly_linked_list.cpp
+++ Sem_2/singly_linked_list/singly_linked_list.cpp
@@ -8,38 +8,37 @@ struct Node
 	Node(const char& _data) : data(_data), next(nullptr) {}
 };
 
-struct single_linked_list
+struct singly_linked_list
 {
 	Node* head;
 	Node* tail;
-
 	int size;
+	bool is_init = false;
 
-	single_linked_list() : head(nullptr), tail(nullptr), size(0) {}
-	
+	singly_linked_list(const int n) : head(nullptr), tail(nullptr), size(0), is_init(false) { init(n); }
+
 	void init(const int n)
 	{
-		char tmp;
-
-		if (n < 1)
+		if (is_init) 
 		{
-			std::cerr << "List size must be at least 1 !" << std::endl;
-			return; 
+			std::cerr << "List has already been initialized !" << std::endl;
+			return;
 		}
-
+		char data;
 		for (int i = 0; i < n; ++i)
 		{
-			std::cin >> tmp;
-			push_back(tmp);
+			std::cin >> data;
+			push_back(data);
 		}
-		size = n;
+		is_init = true;
 	}
+
+	bool is_empty() const { return head == nullptr; }
 
 	void print()
 	{
-		if (is_empty()) return;
 		Node* node = head;
-		while (node)
+		while (node != nullptr)
 		{
 			std::cout << node->data << ' ';
 			node = node->next;
@@ -47,111 +46,111 @@ struct single_linked_list
 		std::cout << std::endl;
 	}
 
-	bool is_empty() { return head == nullptr; } 
-
-	void push_back(const char& _data) 
-	{
-		Node* node = new Node(_data);
-		
-		if (is_empty())
-		{
-			head = node;
-			tail = node;
-			return;
-		}
-		tail->next = node;
-		tail = node;
-
-		++size;
-	} 
-
-	void remove_back() 
-	{
-		if (is_empty()) { return; }
-
-		if(head == tail) 
-		{
-			delete head;
-			head = tail = nullptr;
-			return;
-		}
-
-		Node* current = head;
-		while (current->next != tail)
-		{
-			current = current->next;
-		}
-		delete tail;
-		tail = current;
-		tail->next = nullptr;
-
-		--size;
-	}
-
 	void push_front(const char& _data)
 	{
 		Node* node = new Node(_data);
 		if (is_empty())
 		{
-			head = node;
-			tail = node;
-			return;
+			head = tail = node;
 		}
-		node->next = head;
-		head = node;
-
+		else
+		{
+			node->next = head;
+			head = node;
+		}
 		++size;
 	}
 
-	void remove_front() 
+	void push_back(const char& _data)
 	{
-		if (is_empty()) { return; }
+		Node* node = new Node(_data);
+		if (is_empty())
+		{
+			head = tail = node;
+		}
+		else
+		{
+			tail->next = node;
+			tail = node;
+		}
+		++size;
+	}
 
+	void remove_front()
+	{
+		if (is_empty())
+		{
+			std::cerr << "List is empty. Cannot remove from front !" << std::endl;
+			return;
+		}
 		if (head == tail)
 		{
 			delete head;
 			head = tail = nullptr;
+		}
+		else
+		{
+			Node* node = head;
+			head = node->next;
+			delete node;
+		}
+		--size;
+	}
+
+	void remove_back()
+	{
+		if (is_empty())
+		{
+			std::cerr << "List is empty. Cannot remove from back !" << std::endl;
 			return;
 		}
-
-		Node* node = head;
-		head = node->next;
-		delete node;
-
+		if (head == tail)
+		{
+			delete head;
+			head = tail = nullptr;
+		}
+		else
+		{
+			Node* current = head;
+			while (current->next != tail)
+			{
+				current = current->next;
+			}
+			delete tail;
+			tail = current;
+			tail->next = nullptr;
+		}
 		--size;
 	}
 
 	void insert(const char& _data, int pos)
 	{
 		--pos;
-
-		if (pos < 0 || pos > size) { return; }
-
+		if (pos < 0 || pos > size)
+		{
+			std::cerr << "Invalid position !" << std::endl;
+			return;
+		}
 		if (pos == 0)
 		{
 			push_front(_data);
 			return;
 		}
-
 		if (pos == size)
 		{
 			push_back(_data);
 			return;
 		}
-
 		Node* node = new Node(_data);
 		Node* current = head;
-
 		for (int i = 0; i < pos - 1; ++i)
 		{
 			current = current->next;
 		}
-
 		node->next = current->next;
 		current->next = node;
-
 		size++;
-	} 
+	}
 
 	void multiple_insert(int pos, const int n)
 	{
@@ -159,42 +158,38 @@ struct single_linked_list
 		{
 			char _data;
 			std::cin >> _data;
-
 			insert(_data, pos);
-
 			++pos;
 		}
 	}
 
-	void remove(int pos) 
+	void remove(int pos)
 	{
-		if (pos < 1 || pos > size) { return; }
-
+		if (pos < 1 || pos > size)
+		{
+			std::cerr << "Invalid position !" << std::endl;
+			return;
+		}
 		if (pos == 1)
 		{
 			remove_front();
 			return;
 		}
-
 		if (pos == size)
 		{
 			remove_back();
 			return;
 		}
-
 		Node* current = head;
 		Node* previous = nullptr;
-
 		for (int i = 1; i < pos; ++i)
 		{
 			previous = current;
 			current = current->next;
 		}
-
 		previous->next = current->next;
 		delete current;
 		current = nullptr;
-
 		--size;
 	}
 
@@ -211,24 +206,38 @@ struct single_linked_list
 
 	const char& find_value(const char& _data)
 	{
-		Node* current = head;
-		while (current != nullptr)
+		if (is_empty())
 		{
-			if (current->data == _data) { return current->data; }
-			current = current->next;
+			std::cerr << "List is empty. Cannot find value !" << std::endl;
+			return ' ';
 		}
-		return ' ';
+		else
+		{
+			Node* current = head;
+			while (current != nullptr)
+			{
+				if (current->data == _data) { return current->data; }
+				current = current->next;
+			}
+		}
 	}
 
 	Node* find_value_ptr(const char& _data)
 	{
-		Node* current = head;
-		while (current != nullptr)
+		if (is_empty())
 		{
-			if (current->data == _data) { return current; }
-			current = current->next;
+			std::cerr << "List is empty. Cannot find value !" << std::endl;
+			return nullptr;
 		}
-		return nullptr;
+		else
+		{
+			Node* current = head;
+			while (current != nullptr)
+			{
+				if (current->data == _data) { return current; }
+				current = current->next;
+			}
+		}
 	}
 };
 
